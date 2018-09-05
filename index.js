6e3f1f0a2f2c6651b12fbbb9386dda706d69b2e4
Jul Jen <juliajen.prog@gmail.com>
2018-09-05 22:08:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Create a constructor function for each of the following:
-// 
+//
 // Scooter with year, color, and model properties
 // Driver with name, age, and experience properties
 // PickupLocation with address and city properties
@@ -8,4 +8,15 @@ function Scooter(year, color, model) {
   this.year = year
   this.color = color
   this.model = model
+}
+
+function Driver(name, age, experience) {
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
+
+function PickupLocation(address, city) {
+  this.address = address
+  this.city = city
 }
